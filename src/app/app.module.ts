5ec29493cf534bbcd8d721b9b1a3f9f28bfaeb35
Florian Sprenger <61649755+Nairo05@users.noreply.github.com>
2023-03-19 18:15:12 +0100

    SecurtiyTest1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClient, HttpClientModule, HttpClientXsrfModule} from "@angular/common/http";
+import {httpInterceptorProviders} from "./components/interceptors/test";
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+    ],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/DataService.ts
@@ -0,0 +1,25 @@
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+
+const BACKEND_API = "https://eventhorizonbackend.azurewebsites.net/";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+
+  constructor(private http: HttpClient) {}
+
+  privateping(): Observable<any> {
+    return this.http.get<any>(
+      BACKEND_API + 'private-test/ping',
+      httpOptions
+    );
+  }
+
+}
