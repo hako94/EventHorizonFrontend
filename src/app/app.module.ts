e06e75b8c32a0eba27f1be90f5a86eff96924ace
lukasmlr <i21019@hb.dhbw-stuttgart.de>
2023-03-22 21:57:48 +0100

    configure angular material and add header and dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import {FormsModule} from "@angular/forms";
-import {HttpClient, HttpClientModule, HttpClientXsrfModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {httpInterceptorProviders} from "./interceptors/BearerInterceptor";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent
   ],
     imports: [
         BrowserModule,
