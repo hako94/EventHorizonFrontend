138bcc47cf268a0e54ee963109c0b6e33c95dce6
Florian Sprenger <61649755+Nairo05@users.noreply.github.com>
2023-04-23 22:45:19 +0200

    implemented 1.3.3 Automatische Abmeldung eines Nutzers

--- src/app/interceptors/BearerInterceptor.ts
+++ src/app/interceptors/BearerInterceptor.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HTTP_INTERCEPTORS,
+  HttpErrorResponse
+} from '@angular/common/http';
+import {catchError, Observable, throwError} from 'rxjs';
 import {StorageService} from "../services/StorageService";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
 
-  constructor(private storageService : StorageService) {
+  constructor(private storageService : StorageService, private router : Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -17,7 +25,15 @@ export class HttpRequestInterceptor implements HttpInterceptor {
       headers: req.headers.set('Authorization', jwtString)
     });
 
-    return next.handle(req);
+    return next.handle(req).pipe(
+      catchError(error => {
+        if (error instanceof HttpErrorResponse && error.status == 401 && !req.url.includes("login")) {
+          this.storageService.clear()
+          this.router.navigate(['/login'], { queryParams: {error: 'sessionExpired', locationToRedirectAfterLogin: 'null'}});
+        }
+        return throwError(() => error);
+      })
+    );
   }
 }
 
