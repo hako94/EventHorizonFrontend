2bfeb012feaf7818d2c31e253c0ac11757e95c46
lukasmlr <i21019@hb.dhbw-stuttgart.de>
2023-05-02 21:17:34 +0200

    add autocomplete for event invite

--- src/app/components/organizationview/organization-event-details-view/event-attender-view/event-attender-view.component.ts
+++ src/app/components/organizationview/organization-event-details-view/event-attender-view/event-attender-view.component.ts
@@ -1,32 +1,46 @@
-import {Component, Input} from '@angular/core';
-import {UserAtEventModel} from "../../../../models/UserAtEventModel";
+import {Component, Input, OnInit} from '@angular/core';
 import {DataService} from "../../../../services/DataService";
 import {StorageService} from "../../../../services/StorageService";
-import {MatCheckboxChange} from "@angular/material/checkbox";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {UserForEventWithRoleModel} from "../../../../models/UserForEventWithRoleModel";
+import {OrganizationUserModel} from "../../../../models/OrganizationUserModel";
+import {map, Observable, startWith} from "rxjs";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-event-attender-view',
   templateUrl: './event-attender-view.component.html',
   styleUrls: ['./event-attender-view.component.scss']
 })
-export class EventAttenderViewComponent {
+export class EventAttenderViewComponent implements OnInit {
   @Input() orgaID = '';
   @Input() eventID = '';
   @Input() roleIdInEvent!: number;
 
-  ownRoleInOrg : number = -1;
-  attendee : UserForEventWithRoleModel[] = [];
-  editMode : boolean = false;
-  editedUser : string = '';
-  selectedRole : number = 12;
-  invitedEmail : string = '';
-  invitedUser : string = '';
-  inviteLoading : boolean = false;
+  ownRoleInOrg: number = -1;
+  attendee: UserForEventWithRoleModel[] = [];
+  editMode: boolean = false;
+  editedUser: string = '';
+  selectedRole: number = 12;
+  invitedEmail: string = '';
+  invitedUser: string = '';
+  inviteLoading: boolean = false;
+  availableUsersForEventInvite: OrganizationUserModel[] = [];
+  filteredUsersForEventInvite?: Observable<OrganizationUserModel[]>;
+  eventInviteControl = new FormControl<OrganizationUserModel>({
+    id: '',
+    vorname: '',
+    nachname: '',
+    email: '',
+    role: {
+      id: 0,
+      role: ''
+    }
+  });
 
   constructor(private dataService: DataService, private storageService: StorageService, private snackBar: MatSnackBar) {
   }
+
   ngOnInit(): void {
     console.log("ngonoinit");
     this.attendee = [];
@@ -37,8 +51,27 @@ export class EventAttenderViewComponent {
         }
       });
     });
+    this.availableUsersForEventInvite = [];
+    this.dataService.getOrganizationMember(this.orgaID).subscribe(success => {
+      success.forEach(member => {
+        if (member.email != null) {
+          this.availableUsersForEventInvite.push(member)
+        }
+      })
+    });
+    this.loadOptions();
     this.ownRoleInOrg = this.storageService.getRoleInCurrentOrganization(this.orgaID);
     console.log("Rolle:" + this.roleIdInEvent);
+  }
+
+  loadOptions(): void {
+    this.filteredUsersForEventInvite = this.eventInviteControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const email = typeof value === 'string' ? value : value;
+        return email ? this._filterEmail(email.toString()) : this.availableUsersForEventInvite.slice();
+      }),
+    );
   }
 
 
@@ -47,8 +80,22 @@ export class EventAttenderViewComponent {
    *
    * @param roleId
    */
-  hasRole(roleId: number) : boolean {
+  hasRole(roleId: number): boolean {
     return this.storageService.getRoleInCurrentOrganization(this.orgaID) == roleId;
+  }
+
+  displayMemberEmail(member: OrganizationUserModel): string {
+    return member && member.email ? member.email : '';
+  }
+
+  private _filterEmail(email: string): OrganizationUserModel[] {
+    const filterValue: string = email.toLowerCase();
+    return this.availableUsersForEventInvite.filter(filteredMember => filteredMember.email.toLowerCase().includes(filterValue));
+  }
+
+  private requireMatch(input: string): boolean {
+    let validOptions = this.availableUsersForEventInvite.map(members => members.email);
+    return validOptions.includes(input);
   }
 
   enableEdit(id: string) {
@@ -59,7 +106,7 @@ export class EventAttenderViewComponent {
     }
   }
 
-  saveAttenderRole(orgId: string, attenderId: string, role: number){
+  saveAttenderRole(orgId: string, attenderId: string, role: number) {
     console.log("saved new role " + role + "for attender " + attenderId);
     this.dataService.saveAttenderRole(orgId, this.eventID, attenderId, role).subscribe(() => {
       this.ngOnInit();
@@ -72,7 +119,7 @@ export class EventAttenderViewComponent {
     this.selectedRole = 12;
   }
 
-  deleteAttender(attenderEmail: string) : void {
+  deleteAttender(attenderEmail: string): void {
     console.log("deleted attender " + attenderEmail);
     this.dataService.deleteAttenderFromEvent(this.orgaID, this.eventID, attenderEmail).subscribe(success => {
       this.snackBar.open('Teilnehmer abgemeldet', 'OK', {duration: 3000});
@@ -82,16 +129,22 @@ export class EventAttenderViewComponent {
     });
   }
 
-  inviteSubmit() : void {
+  inviteSubmit(): void {
     this.inviteLoading = true;
-    console.log(this.selectedRole)
-    this.dataService.inviteUserToEvent(this.orgaID, this.eventID, this.invitedEmail, this.selectedRole).subscribe(success => {
-      this.invitedUser = success;
-      this.snackBar.open('Einladung wurde erfolgreich versandt', 'OK', {duration: 3000});
+    if (this.requireMatch(this.eventInviteControl.value!.email)) {
+      console.log(this.selectedRole);
+      console.log(this.eventInviteControl.value!.id);
+      this.dataService.inviteUserToEvent(this.orgaID, this.eventID, this.eventInviteControl.value!.id, this.selectedRole).subscribe(success => {
+        this.invitedUser = success;
+        this.snackBar.open('Einladung wurde erfolgreich versandt', 'OK', {duration: 3000});
+        this.inviteLoading = false;
+      }, error => {
+        this.snackBar.open('Es ist ein Fehler aufgetreten', 'OK', {duration: 3000});
+        this.inviteLoading = false;
+      })
+    } else {
+      this.snackBar.open('Email muss aus in den Vorschlägen vorhanden sein', 'Ok', {duration: 3500});
       this.inviteLoading = false;
-    }, error => {
-      this.snackBar.open('Es ist ein Fehler aufgetreten', 'OK', {duration: 3000});
-      this.inviteLoading = false;
-    })
+    }
   }
 }
