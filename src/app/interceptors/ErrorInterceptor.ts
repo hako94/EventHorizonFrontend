ad06a55ee53d9b6e32855bd2b5a3878bd3caf69f
Hannes Kollert <hako94@web.de>
2023-05-03 18:52:46 +0200

    activate ErrorInterceptor for ErrorMessages

--- src/app/interceptors/ErrorInterceptor.ts
+++ src/app/interceptors/ErrorInterceptor.ts
@@ -16,20 +16,21 @@ export class ErrorInterceptor implements HttpInterceptor {
           let errorMessage = ''; // Nachricht, die an den Nutzer angezeigt wird
           if (error.error instanceof ErrorEvent) {
             // Client-seitiger Fehler
-            errorMessage = `Error: ${error.error.message}`;
+            //errorMessage = `Error: ${error.error.message}`;
           } else {
             // Server-seitiger Fehler
             if (error.error.message) {
               errorMessage = `Fehler: ${error.error.message}`;
-            } else {
-              errorMessage = `${error.status}: ${error.message}`;
+            }
+            else {
+              // errorMessage = `${error.status}: ${error.message}`;
             }
           }
 
-          //this.snackBar.open(errorMessage, 'OK', {
-          //  duration: 5000,
-          //  panelClass: ['mat-toolbar', 'mat-warn']
-          //});
+          this.snackBar.open(errorMessage, 'OK', {
+          duration: 5000,
+          panelClass: ['mat-toolbar', 'mat-warn']
+          });
 
           return throwError(error);
         })
